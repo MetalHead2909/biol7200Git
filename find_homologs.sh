3b91d0037fe14a303ba7ddaebbdb32f88a611362
amey agarwal <ameyagarwal10@gmail.com>
2025-09-07 16:08:46 -0400

    modified find_homologs script

--- find_homologs.sh
+++ find_homologs.sh
@@ -2,6 +2,6 @@ query_file=$1
 subject_file=$2
 output_file=$3
 
-tblastn -query $query_file -subject $subject_file -task tblastn -outfmt '6 std sseq' -gapopen 6 -gapextend 2 -qcov_hsp_perc 90 -out $output_file
+tblastn -query $query_file -subject $subject_file -task tblastn -outfmt '6 pident length qlen' -out $output_file
 
-wc -l $output_file | awk '{print $1}'
+awk -F'\t' '{if (($1>30) && ((($2)/($3))>0.9)) print $1, $2, $3}' $output_file | wc -l | awk '{print $1}'
